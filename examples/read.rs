1258693f89590e965ea5c2434fb8560550640e19
storycraft <storycraft@pancake.sh>
2026-03-09 20:40:34 +0900

    chore: pretty print

--- examples/read.rs
+++ examples/read.rs
@@ -5,7 +5,7 @@ use std::{fs::File, io::BufReader};
 fn main() -> Result<(), Box<dyn Error>> {
     let mut file = BufReader::new(File::open("01_com_001_01.ks.scn")?);
     let mut psb = PsbFile::open(&mut file)?;
-    println!("{:?}", psb.deserialize_root::<PsbValue>()?);
+    println!("{:#?}", psb.deserialize_root::<PsbValue>()?);
 
     Ok(())
 }
