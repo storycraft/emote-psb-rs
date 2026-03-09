930eb6fb73795b44df4534eee85a8914d3f04c99
storycraft <storycraft@pancake.sh>
2026-03-10 00:08:42 +0900

    fix: sample

--- examples/read.rs
+++ examples/read.rs
@@ -3,7 +3,7 @@ use emote_psb::{psb::read::PsbFile, value::PsbValue};
 use std::{fs::File, io::BufReader};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut file = BufReader::new(File::open("[DXT1][win]aka14.psb")?);
+    let mut file = BufReader::new(File::open("sample.psb")?);
     let mut psb = PsbFile::open(&mut file)?;
     dbg!(&psb);
     println!("{:#?}", psb.deserialize_root::<PsbValue>()?);
